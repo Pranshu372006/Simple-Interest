fbe897493d07a9e5ce547a760fbd3eb937676842
Pranshu372006 <164176692+Pranshu372006@users.noreply.github.com>
2024-10-10 11:11:14 +0530

    FIND SIMPLE INTEREST IN FLAOT IN TURBO C++
    C PROGRAMMING

---
+++ FLAOT.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+flaot p;
+flaot r;
+flaot t;
+flaot si;
+clrscr();
+printf("enter the value of p =");
+scanf("%f",p);
+printf("enter the value of r =");
+scanf("%f",r);
+printf("enter the value of t =");
+scanf("%f",t);
+si =p*r*t/100.0;
+printf("Simple Ineterest =%f",si);
+getch();
+return 0;
+}
