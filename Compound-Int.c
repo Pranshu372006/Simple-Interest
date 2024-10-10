21852462bbdbdbd4e59f4e671ef539ed779428b2
Pranshu372006 <164176692+Pranshu372006@users.noreply.github.com>
2024-10-10 11:47:31 +0530

    Compound Interest in int by vs code
    C Progamming

---
+++ Compound-Int.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int p;
+  int r;
+  int t;
+  int ci;
+printf("enter the value of p =");
+scanf("%d",p);
+printf("enter the value of r =");
+scanf("%d",r);
+printf("enter the value of t =");
+scanf("%d",t);
+ci =p*(pow(1+r/100.,t)-1);
+printf("Compound Interest =%d",ci);
+return 0;
+}
